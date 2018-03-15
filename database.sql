0427e261cdbecf5a08b7a0b4068be92cca7cafcf
Krayon <krayon.git@qdnx.org>
2018-03-15 16:28:01 +1100

    Support older MySQL and only latin1 charset

--- database.sql
+++ database.sql
@@ -3,10 +3,10 @@
 #
 CREATE TABLE urls (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    url VARCHAR(1000) NOT NULL,
+    url VARCHAR(767) NOT NULL,
     created DATETIME NOT NULL,
     accessed DATETIME,
     hits INT UNSIGNED NOT NULL DEFAULT 0,
     PRIMARY KEY (id),
-    UNIQUE (url)
-);
+    UNIQUE (url(767))
+) CHARACTER SET latin1;
